a715732fb9159fd844b40617c7dc3854cf247057
Toto <2429743@qq.com>
2022-06-25 14:45:07 +0800

    Test1

---
+++ src/main/java/com/lyh/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.lyh.blog.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 统一异常处理类
+ */
+
+
+//拦截所有Controller控制器的异常
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //配置Exception级别的异常
+    @ExceptionHandler(Exception.class)
+    //返回错误页面
+    public ModelAndView exceptionHander(HttpServletRequest request,Exception e){
+
+        logger.error("Request URL : {},Exception : {}",request.getRequestURL());
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url",request.getRequestURL());
+        mv.addObject("exception",e);
+        mv.setViewName("error/error");
+        return null;
+    }
+}
